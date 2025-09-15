2ac1830f1171168c655127558feded14ba154fcb
Hendra <etik.hendra@gmail.com>
2025-09-16 06:56:33 +0700

    making a side bar

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,18 +1,41 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
-import Logo from '../assets/Vector2.svg'
-import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  GridItem,
+  Image,
+  List,
+  ListItem,
+  VStack,
+} from "@chakra-ui/react";
+import Logo from "../assets/Vector2.svg";
+import Dompet from "../assets/Dompet.svg";
 
-const MainPage = () => {
+interface transaksi{
 
-  return (
-    <Container>
-        <Box position="fixed" display="flex"  left="30px" mt="20px" bottom="-1">
-            <Flex justifyContent="center">
-            <img  src={Logo}/>
-            </Flex>
-        </Box>
-    </Container>
-  )
 }
 
-export default MainPage
+const MainPage = () => {
+  return (
+    <>
+      <Grid position="fixed" display="flex" mt="20px">
+        <VStack paddingLeft={10}>
+        <Image src={Logo} />
+        <List paddingY="5px">
+          <ListItem>
+            <Button width="100%" borderRadius={0}bgColor='#1C4532'  _hover={{bgColor:"#947f7f4b"}}>
+              <VStack>
+                <Image src={Dompet} />
+              </VStack>
+            </Button>
+          </ListItem>
+        </List>
+        </VStack>
+      </Grid>
+      <Container></Container>
+    </>
+  );
+};
+
+export default MainPage;
