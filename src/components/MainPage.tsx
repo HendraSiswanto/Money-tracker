28cc9082591b9b98cce805fee187d248549d9ee5
Hendra <etik.hendra@gmail.com>
2025-09-17 06:58:13 +0700

    centered, and adjust onclick simulation

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,8 @@
 import {
-  Box,
   Button,
+  Text,
   Container,
-  Grid,
-  GridItem,
+  Flex,
   Image,
   List,
   ListItem,
@@ -11,28 +10,49 @@ import {
 } from "@chakra-ui/react";
 import Logo from "../assets/Vector2.svg";
 import Dompet from "../assets/Dompet.svg";
+import { useState } from "react";
 
-interface transaksi{
-
-}
 
 const MainPage = () => {
+
+  const [transaction,setTransaction] = useState(false)
   return (
     <>
-      <Grid position="fixed" display="flex" mt="20px">
-        <VStack paddingLeft={10}>
-        <Image src={Logo} />
+      <Flex
+        gap={5}
+        flexDirection="column"
+        width="90px"
+        alignItems="center"
+        position="fixed"
+        mt="20px"
+      >
+        <Image boxSize="28px" src={Logo} />
         <List paddingY="5px">
           <ListItem>
-            <Button width="100%" borderRadius={0}bgColor='#1C4532'  _hover={{bgColor:"#947f7f4b"}}>
+            <Button
+              borderRadius={0}
+              bgColor={transaction? "#947f7f4b" : "#1C4532"}
+              height="80px"
+              onClick={() => setTransaction(true)}
+              _hover={{
+                borderLeft: "4px",
+                justifyContent: "center",
+                bgColor: "#947f7f4b",
+                width: "100px",
+                borderRight: "4px",
+                borderRightColor: "#947F7F",
+              }}
+            >
               <VStack>
                 <Image src={Dompet} />
+                <Text color="#a2acbaff" fontWeight="medium" fontSize="12px">
+                  Transactions
+                </Text>
               </VStack>
             </Button>
           </ListItem>
         </List>
-        </VStack>
-      </Grid>
+      </Flex>
       <Container></Container>
     </>
   );
