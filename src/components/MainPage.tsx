b972e2bc9cc97c189ad7b01cfec9665a792014f2
Hendra <etik.hendra@gmail.com>
2025-09-25 06:43:13 +0700

    adjust for sidebar, make the type fixed

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -9,64 +9,67 @@ import {
   VStack,
   Box,
 } from "@chakra-ui/react";
-import useImage, {type Sided} from "../hooks/useImage";
+import useImage, { type Sided } from "../hooks/useImage";
 import Logo from "../assets/Vector2.svg";
 
 interface Props {
-  onSelectImage: (imageData: Sided) => void,
-  selectImage: Sided | null
+  onSelectImage: (imageData: Sided) => void;
+  selectImage: Sided | null;
 }
 
-const MainPage = ({onSelectImage,selectImage}:Props) => {
-  const {data} = useImage();
+const MainPage = ({ onSelectImage, selectImage }: Props) => {
+  const { data } = useImage();
 
   return (
     <>
-      <Flex
-      h="100vh" w="100%"
-      >
-        <Box 
-        maxWidth="7%"
-        justifyItems="center"
-        flex="1"
-        gap={5}
-        flexDirection="column"
-        alignItems="center"
-        mt="20px">
-        <Image boxSize="28px" src={Logo} />
-        <List paddingY="5px">
-
-        {data.map((imageData) => (
-          <ListItem key={imageData.id}>
-            <Button
-              width="100%"
-              borderRadius={0}
-              bgColor={imageData.id === selectImage?.id? "#947f7f4b" : "#1C4532"}
-              borderRight={imageData.id === selectImage?.id? "4px solid #947F7F" : ""}
-              
-              height="80px"
-              onClick={() => onSelectImage(imageData)}
-              _hover={{
-             
-                bgColor: "#947f7f4b",
-                width: "100px",
-               
-              }}
-            >
-              <VStack>
-                <Image src={imageData.imageUrl} />
-                <Text color="#a2acbaff" fontWeight="medium" fontSize="12px">
-                  {imageData.name}
-                </Text>
-              </VStack>
-            </Button>
-          </ListItem>
-        ))}
-        </List>
+      <Flex position="fixed" h="100vh" w="100%">
+        <Box
+          maxWidth="7%"
+          justifyItems="center"
+          flex="1"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <Image mt="20px" boxSize="28px" src={Logo} />
+          <List mt={6} paddingY="5px">
+            {data.map((imageData) => (
+              <ListItem key={imageData.id}>
+                <Button
+                  width="100px"
+                  borderRadius={0}
+                  bgColor={
+                    imageData.id === selectImage?.id ? "#947f7f4b" : "#1C4532"
+                  }
+                  borderRight={
+                    imageData.id === selectImage?.id ? "4px solid #947F7F" : ""
+                  }
+                  borderLeft={
+                    imageData.id === selectImage?.id
+                      ? "4px solid transparent"
+                      : ""
+                  }
+                  height="80px"
+                  onClick={() => onSelectImage(imageData)}
+                  _hover={{
+                    bgColor: "#947f7f4b",
+                    width: "100px",
+                  }}
+                >
+                  <VStack>
+                    <Image src={imageData.imageUrl} />
+                    <Text color="#a2acbaff" fontWeight="medium" fontSize="12px">
+                      {imageData.name}
+                    </Text>
+                  </VStack>
+                </Button>
+              </ListItem>
+            ))}
+          </List>
         </Box>
-         <Box flex={1} bgColor="#F2F2F6" maxWidth="100%">tes</Box>
+        <Box flex={1} bgColor="#f2f2f6fa" maxWidth="100%">
+          tes
+        </Box>
       </Flex>
-     
     </>
   );
 };
