2ecccbf3fc23f8456397f4812c68c15c0d18912b
Hendra <etik.hendra@gmail.com>
2025-10-10 06:40:11 +0700

    start making input on income

---
+++ src/data/dataIncome.ts
@@ -0,0 +1,7 @@
+export default [
+    { "id": 1, "in": "Salary","emote": "💼"},
+    { "id": 2, "in": "Bonus","emote": "💰"},
+    { "id": 3, "in": "Freelance","emote": "👨‍💻"},
+    { "id": 4, "in": "Investment","emote": "📈"},
+    { "id": 5, "in": "Gift","emote": "🎁"},
+  ];
