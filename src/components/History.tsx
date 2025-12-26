089d9949dc440b6881e339fc2cd9058abe8930bc
Hendra <etik.hendra@gmail.com>
2025-12-26 20:54:24 +0700

    remove the old one

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,16 +1,8 @@
 import {
   Box,
   Button,
-  Table,
-  Tbody,
-  Td,
-  Tfoot,
-  Th,
-  Thead,
-  Tr,
   Text,
   Tooltip,
-  Icon,
   AlertDialog,
   AlertDialogBody,
   AlertDialogFooter,
@@ -91,8 +83,6 @@ const History: React.FC = () => {
     });
     setEditOpen(true);
   };
-  const formatOutcome = (outcome: string) =>
-    outcome.charAt(0).toUpperCase() + outcome.slice(1);
 
   const saveEditData = async () => {
     if (!editData) return;
@@ -178,7 +168,7 @@ const History: React.FC = () => {
         <Flex justify="space-between" mb={6} gap={4} wrap="wrap">
           <Heading
             size="lg"
-            bgGradient="linear(to-r, #1C4532, #45241cd4)"
+            bgGradient="linear(to-r, #54bd8cff, #45241cd4)"
             bgClip="text"
           >
             Transaction History
@@ -218,287 +208,128 @@ const History: React.FC = () => {
           </Flex>
         </Flex>
         <Stack spacing={4}>
-          {transactions.map((item) => (
-            <Box
-              key={item.id}
-              p={4}
-              border="1px solid"
-              borderColor="gray.300"
-              borderRadius="lg"
-              _hover={{ boxShadow: "md" }}
-            >
-              <Flex justify="space-between" gap={5} align="center">
-                <Flex flexDir="column" gap={1}>
-                  <Flex justify="center">
-                    <Badge
-                      colorScheme={item.outcome === "income" ? "green" : "red"}
-                      color={
-                        item.outcome === "income" ? "#1C4532" : "#45241cff"
-                      }
-                      px={3}
-                      py={1}
-                      mb={1}
-                      borderRadius="full"
-                      textTransform="capitalize"
+          {transactions.length > 0 ? (
+            <>
+              {isLoading ? (
+                <HisSkeleton />
+              ) : (
+                <>
+                  {transactions.map((item) => (
+                    <Box
+                      key={item.id}
+                      p={4}
+                      border="1px solid"
+                      borderColor="gray.300"
+                      borderRadius="lg"
+                      _hover={{ boxShadow: "md" }}
                     >
-                      {item.outcome.toUpperCase()}
-                    </Badge>
-                  </Flex>
+                      <Flex justify="space-between" gap={5} align="center">
+                        <Flex flexDir="column" gap={1}>
+                          <Flex justify="center">
+                            <Badge
+                              colorScheme={
+                                item.outcome === "income" ? "green" : "red"
+                              }
+                              color={
+                                item.outcome === "income"
+                                  ? "#1C4532"
+                                  : "#45241cff"
+                              }
+                              px={3}
+                              py={1}
+                              mb={1}
+                              borderRadius="full"
+                              textTransform="capitalize"
+                            >
+                              {item.outcome.toUpperCase()}
+                            </Badge>
+                          </Flex>
 
-                  <Text fontWeight="bold" fontSize="sm" color="gray.500">
-                    {item.type}
-                  </Text>
-                  <Text fontSize="sm" color="gray.500">
-                    {new Date(item.date).toLocaleDateString("id-ID")}
-                  </Text>
+                          <Text
+                            fontWeight="bold"
+                            fontSize="sm"
+                            color="gray.500"
+                          >
+                            {item.type}
+                          </Text>
+                          <Text fontSize="sm" color="gray.500">
+                            {new Date(item.date).toLocaleDateString("id-ID")}
+                          </Text>
 
-                  {item.note && (
-                    <Tooltip label={item.note}>
-                      <Text
-                        fontSize="sm"
-                        fontWeight="bold"
-                        color="gray.500"
-                        isTruncated
-                        maxW="180px"
-                      >
-                        {item.note}
-                      </Text>
-                    </Tooltip>
-                  )}
-                </Flex>
+                          {item.note && (
+                            <Tooltip label={item.note}>
+                              <Text
+                                fontSize="sm"
+                                fontWeight="bold"
+                                color="gray.500"
+                                isTruncated
+                                maxW="180px"
+                              >
+                                {item.note}
+                              </Text>
+                            </Tooltip>
+                          )}
+                        </Flex>
 
-                <Flex align="center" gap={3}>
-                  <Text
-                    fontWeight="bold"
-                    color={item.outcome === "income" ? "#1C4532" : "#45241cff"}
+                        <Flex align="center" gap={3}>
+                          <Text
+                            fontWeight="bold"
+                            color={
+                              item.outcome === "income"
+                                ? "#1C4532"
+                                : "#45241cff"
+                            }
+                          >
+                            {rupiahFormat.format(item.amount)}
+                          </Text>
+
+                          <IconButton
+                            bgColor="green.100"
+                            color="green.600"
+                            _hover={{ bg: "green.50" }}
+                            aria-label="Edit"
+                            icon={<BsPenFill />}
+                            size="sm"
+                            onClick={() => handleEdit(item)}
+                          />
+
+                          <IconButton
+                            aria-label="Delete"
+                            icon={<BsTrash3Fill />}
+                            size="sm"
+                            colorScheme="red"
+                            onClick={() => {
+                              if (item.id !== undefined) {
+                                handleOpenDialog(item.id);
+                              }
+                            }}
+                          />
+                        </Flex>
+                      </Flex>
+                    </Box>
+                  ))}
+                  <Box
+                    p={4}
+                    border="1px solid"
+                    borderColor="green.300"
+                    borderRadius="lg"
+                    bg="green.50"
                   >
-                    {rupiahFormat.format(item.amount)}
-                  </Text>
-
-                  <IconButton
-                    bgColor="green.100"
-                    color="green.600"
-                    _hover={{ bg: "green.50" }}
-                    aria-label="Edit"
-                    icon={<BsPenFill />}
-                    size="sm"
-                    onClick={() => handleEdit(item)}
-                  />
-
-                  <IconButton
-                    aria-label="Delete"
-                    icon={<BsTrash3Fill />}
-                    size="sm"
-                    colorScheme="red"
-                    onClick={() => {
-                      if (item.id !== undefined) {
-                        handleOpenDialog(item.id);
-                      }
-                    }}
-                  />
-                </Flex>
-              </Flex>
-            </Box>
-          ))}
-          <Box
-            p={4}
-            border="1px solid"
-            borderColor="green.300"
-            borderRadius="lg"
-            bg="green.50"
-          >
-            <Flex justify="space-between">
-              <Text fontWeight="bold">Balance</Text>
-              <Text fontWeight="bold">{rupiahFormat.format(balance)}</Text>
-            </Flex>
-          </Box>
+                    <Flex justify="space-between">
+                      <Text fontWeight="bold">Balance</Text>
+                      <Text fontWeight="bold">
+                        {rupiahFormat.format(balance)}
+                      </Text>
+                    </Flex>
+                  </Box>
+                </>
+              )}
+            </>
+          ) : (
+            ""
+          )}
         </Stack>
       </Container>
-
-      {transactions.length > 0 ? (
-        <>
-          {isLoading ? (
-            <HisSkeleton />
-          ) : (
-            <>
-              <Box display="flex" justifyContent="center" mt={6} mb={6}>
-                <Table
-                  style={{ tableLayout: "fixed" }}
-                  size="md"
-                  variant="simple"
-                  width="1200px"
-                >
-                  <Thead>
-                    <Tr>
-                      <Th
-                        whiteSpace="nowrap"
-                        textAlign="center"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                        w="80px"
-                      >
-                        Action
-                      </Th>
-                      <Th
-                        textAlign="center"
-                        width="15px"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                        w="120px"
-                      >
-                        Income/Expense
-                      </Th>
-                      <Th
-                        textAlign="center"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                        w="140px"
-                      >
-                        Type
-                      </Th>
-                      <Th
-                        textAlign="center"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                        w="140px"
-                      >
-                        Date
-                      </Th>
-                      <Th
-                        textAlign="center"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                        w="200px"
-                      >
-                        Note
-                      </Th>
-                      <Th
-                        textAlign="center"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                        w="160px"
-                      >
-                        Amount
-                      </Th>
-                    </Tr>
-                  </Thead>
-
-                  <Tbody>
-                    {transactions.map((item) => (
-                      <Tr key={item.id}>
-                        <Td
-                          textAlign="center"
-                          border="2px solid #1C4532"
-                          color="#1C4532"
-                        >
-                          <Flex justifyContent="center" gap={2} w="100%">
-                            <Button
-                              bgColor="#45241cff"
-                              _active={{ bgColor: "#45241cd4" }}
-                              _hover={{ bgColor: "#45241cd4" }}
-                              onClick={() => {
-                                if (item.id !== undefined) {
-                                  handleOpenDialog(item.id);
-                                }
-                              }}
-                              w="40px"
-                              h="40px"
-                            >
-                              <Icon
-                                boxSize={5}
-                                as={BsTrash3Fill as React.ElementType}
-                              />
-                            </Button>
-                            <Button
-                              w="40px"
-                              h="40px"
-                              bgColor="#1C4532"
-                              _active={{ bgColor: "#1c4532db" }}
-                              _hover={{ bgColor: "#1c4532db" }}
-                              onClick={() => handleEdit(item)}
-                            >
-                              <Icon
-                                boxSize={5}
-                                as={BsPenFill as React.ElementType}
-                              />
-                            </Button>
-                          </Flex>
-                        </Td>
-                        <Td
-                          textAlign="center"
-                          border="2px solid #1C4532"
-                          color="#1C4532"
-                        >
-                          {" "}
-                          {formatOutcome(item.outcome)}
-                        </Td>
-                        <Td
-                          textAlign="center"
-                          border="2px solid #1C4532"
-                          color="#1C4532"
-                        >
-                          {item.type}
-                        </Td>
-
-                        <Td
-                          textAlign="center"
-                          border="2px solid #1C4532"
-                          color="#1C4532"
-                        >
-                          {new Date(item.date).toLocaleDateString("en-CA")}
-                        </Td>
-                        <Td
-                          textAlign="center"
-                          border="2px solid #1C4532"
-                          color="#1C4532"
-                          wordBreak="break-word"
-                          textOverflow="ellipsis"
-                        >
-                          <Tooltip label={item.note} hasArrow>
-                            <Text isTruncated maxW="160px" mx="auto">
-                              {item.note || "-"}
-                            </Text>
-                          </Tooltip>
-                        </Td>
-                        <Td
-                          textAlign="center"
-                          border="2px solid #1C4532"
-                          color="#1C4532"
-                        >
-                          {rupiahFormat.format(item.amount)}
-                        </Td>
-                      </Tr>
-                    ))}
-                  </Tbody>
-
-                  <Tfoot>
-                    <Tr>
-                      <Td
-                        colSpan={5}
-                        textAlign="right"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                        fontWeight="bold"
-                      >
-                        Total
-                      </Td>
-                      <Td
-                        textAlign="center"
-                        border="2px solid #1C4532"
-                        color="#1C4532"
-                      >
-                        {rupiahFormat.format(balance)}
-                      </Td>
-                    </Tr>
-                  </Tfoot>
-                </Table>
-              </Box>
-            </>
-          )}
-        </>
-      ) : (
-        ""
-      )}
     </>
   );
 };
