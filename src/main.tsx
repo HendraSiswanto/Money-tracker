2ddf19e5239d5d1c238eec37863218b0000d196e
Hendra <etik.hendra@gmail.com>
2026-01-12 20:29:05 +0700

    move the strict outside the router

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <BrowserRouter>
       <ChakraProvider theme={theme}>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
     </BrowserRouter>
