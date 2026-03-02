82b30b6e2539c94c12ed579ae073a1c35624cb66
Hendra <etik.hendra@gmail.com>
2026-03-02 20:54:18 +0700

    registerr debug

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -3,19 +3,25 @@ import { supabase } from "../libs/supabase";
 export async function useRegister(
   name: string,
   email: string,
-  password: string
+  password: string,
 ) {
-  const { data, error } = await supabase.auth.signUp({
+  const { data } = await supabase.auth.signUp({
     email,
     password,
-    options: {
-      data: {
-        name,
-      },
-    },
   });
 
-  if (error) throw error;
-
+  if (data.user) {
+    await fetch("http://localhost:3000/api/create-user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: data.user.email,
+        supabaseId: data.user.id,
+        name,
+      }),
+    });
+  }
   return data;
 }
